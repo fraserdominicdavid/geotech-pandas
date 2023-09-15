3ea6c25a4acc7a67ed8d264d3e570582ef5173f8
Fraser Dominic David <77269589+fraserdominicdavid@users.noreply.github.com>
2023-09-15 14:16:26 +0800

    feat: add ability to get top depth values (#9)
    Add a `get_top` method to `PointDataFrameAccessor` that can be used to
    get shifted ``Bottom`` depth values as ``Top`` depth values.

--- tests/test_point.py
+++ tests/test_point.py
@@ -23,3 +23,16 @@ def test_get_group():
     df = PointDataFrameAccessor(base_df)
     for point_id in base_df["PointID"].to_list():
         tm.assert_frame_equal(base_df[base_df["PointID"] == point_id], df.get_group(point_id))
+
+
+def test_get_top():
+    """Test if `get_top` returns correct shifted ``Bottom`` depths."""
+    df = PointDataFrameAccessor(
+        pd.DataFrame(
+            {
+                "PointID": ["BH-1", "BH-1", "BH-2", "BH-2"],
+                "Bottom": [1.0, 2.0, 3.0, 4.0],
+            }
+        )
+    )
+    tm.assert_series_equal(df.get_top(), pd.Series([0.0, 1.0, 0.0, 3.0], name="Top"))
