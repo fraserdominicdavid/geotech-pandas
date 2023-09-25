3efcddb311899bf9579e5a86ff7e51a1e6b5c721
Fraser Dominic David <77269589+fraserdominicdavid@users.noreply.github.com>
2023-09-25 08:48:58 +0800

    feat(accessor): register dataframe accessor as `geotech` (#15)
    Registers a DataFrame accessor that provides namespaces to the various
    accessors in Geotech Pandas.

---
+++ tests/test_accessor.py
@@ -0,0 +1,47 @@
+"""Test if accessors are registered with their namespaces as expected."""
+from functools import reduce
+
+import pandas as pd
+import pytest
+
+import geotech_pandas.accessor  # noqa: F401
+from geotech_pandas.accessor import GeotechDataFrameAccessor
+from geotech_pandas.point import PointDataFrameAccessor
+
+
+@pytest.fixture()
+def df():
+    """Return default dataframe for testing."""
+    return pd.DataFrame(
+        {
+            "point_id": ["BH-1", "BH-1", "BH-2", "BH-2"],
+            "bottom": [1.0, 2.0, 3.0, 4.0],
+        }
+    )
+
+
+@pytest.mark.parametrize(
+    ("namespaces", "accessor"),
+    [
+        (["geotech"], GeotechDataFrameAccessor),
+        (["geotech", "point"], PointDataFrameAccessor),
+    ],
+)
+def test_dataframe_accessor(df, namespaces, accessor):
+    """Test if the last element of `namespace` is a registered `accessor` in a `DataFrame` object.
+
+    The `namespace` list is reduced. This means that providing a list of ``["a", "b"]`` would mean
+    that the attribute `a` would first be obtained from `df`, then the attribute `b` would be
+    obtained from `a`. Where `b` would then be checked if it is an instance of `accessor`.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Dataframe checked for registered accessors.
+    namespaces : list of str
+        Namespaces or attributes to be reduced, with the last element checked for the equivalent
+        accessor.
+    accessor : class
+        Equivalent accessor class of the given namespace.
+    """
+    assert isinstance(reduce(getattr, namespaces, df), accessor)
