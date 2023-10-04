17fa75c77c4c8ecd2d856edd0ed0a0db20d27a79
Fraser Dominic David <77269589+fraserdominicdavid@users.noreply.github.com>
2023-10-04 14:55:47 +0800

    docs: initial documentation (#26)
    Initial documentation using [sphinx](https://github.com/sphinx-doc/sphinx)
    applied with [pydata-sphinx-theme](https://github.com/pydata/pydata-sphinx-theme).

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "geotech-pandas"
+copyright = "2023, Fraser Dominic David"
+author = "Fraser Dominic David"
+version = "0.0.0"
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_design"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+html_title = project
+html_theme_options = {
+    "navbar_align": "left",
+}
