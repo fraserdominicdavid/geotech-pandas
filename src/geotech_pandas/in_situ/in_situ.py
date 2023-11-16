f60eda017f8e1a26fe1c1f532882156c187fa6a3
Fraser Dominic David <77269589+fraserdominicdavid@users.noreply.github.com>
2023-11-16 07:53:07 +0000

    feat(in-situ): add in-situ subaccessor
    Add `in-situ` subaccessor that will serve as a namespace for other
    subaccessors that handle in-situ test data.

---
+++ src/geotech_pandas/in_situ/in_situ.py
@@ -0,0 +1,7 @@
+"""General subaccessor for in-situ test subaccessors."""
+
+from geotech_pandas.base import GeotechPandasBase
+
+
+class InSituDataFrameAccessor(GeotechPandasBase):
+    """General subaccessor that contains various subaccessors that handle in-situ test data."""
