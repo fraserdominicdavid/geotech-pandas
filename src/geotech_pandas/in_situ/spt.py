ff8510a166c8057f8ece58eeaecfb42c8d1ee639
Fraser Dominic David <77269589+fraserdominicdavid@users.noreply.github.com>
2023-11-17 13:44:30 +0800

    feat(spt): add ability to get the total penetration of each layer (#61)

--- src/geotech_pandas/in_situ/spt.py
+++ src/geotech_pandas/in_situ/spt.py
@@ -1,7 +1,31 @@
 """Subaccessor that contains SPT-related methods."""
 
+import pandas as pd
+
 from geotech_pandas.base import GeotechPandasBase
 
 
 class SPTDataFrameAccessor(GeotechPandasBase):
     """Subaccessor that contains methods related to the Standard Penetration Test (SPT)."""
+
+    def __init__(self, accessor) -> None:
+        super().__init__(accessor)
+        self._validate_columns(
+            [
+                "sample_type",
+                "sample_number",
+                "blows_1",
+                "blows_2",
+                "blows_3",
+                "pen_1",
+                "pen_2",
+                "pen_3",
+            ]
+        )
+
+    def get_total_pen(self) -> pd.Series:
+        """Return the total penetration of each interval."""
+        return pd.Series(
+            self._obj[["pen_1", "pen_2", "pen_3"]].sum(axis=1, min_count=1),
+            name="total_pen",
+        )
